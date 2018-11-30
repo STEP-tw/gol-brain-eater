deade7be6e0cfe3f2565b02d0c49d8ca33a367b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-30 07:53:58 +0000

    Deploy STEP-tw/gol-brain-eater to github.com/STEP-tw/gol-brain-eater.git:gh-pages

---
+++ precache-manifest.f58aae596ac26581acc8658036153033.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c95c425f2fd8a803a68",
+    "url": "/gol-brain-eater/static/js/runtime~main.2c95c425.js"
+  },
+  {
+    "revision": "2c52b297456400893609",
+    "url": "/gol-brain-eater/static/js/main.2c52b297.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/gol-brain-eater/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "2c52b297456400893609",
+    "url": "/gol-brain-eater/static/css/main.2f2a7cf2.chunk.css"
+  },
+  {
+    "revision": "42ae9e2f054454930b2656d11b55dff6",
+    "url": "/gol-brain-eater/index.html"
+  }
+];
